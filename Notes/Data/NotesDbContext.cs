e76f2057705c9703d781fa358733974eeab13907
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2025-03-01 00:05:16 +0000

    Added constructor to dbcontext

--- Notes/Data/NotesDbContext.cs
+++ Notes/Data/NotesDbContext.cs
@@ -1,8 +1,15 @@
-using System;
+using Microsoft.EntityFrameworkCore;
+using Notes.Models;
 
 namespace Notes.Data;
-
-public class NotesDbContext
+public class NotesDbContext : DbContext
 {
+
+    public NotesDbContext()
+    { }
+    public NotesDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet Notes { get; set; }
 
 }
